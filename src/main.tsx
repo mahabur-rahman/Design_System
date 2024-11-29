2af86b2a78df5091a4fd69ee298af18e48a76d72
mahabur-rahman <annur4395@gmail.com>
2024-11-29 18:54:19 +0600

    Feat: Discover issue with sentry configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://9b78ce7ca64f65b3b6fd40022360146f@o4508249956810752.ingest.us.sentry.io/4508249966706688",
+  integrations: [],
+});
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
+  </StrictMode>
+);
